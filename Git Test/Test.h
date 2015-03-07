2023c81f6e116dfae6391106cbf1a5c7d459f756
Richard Nguyen <Urghh@Richards-MBP.home>
2015-03-07 12:25:17 -0800

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Richard Nguyen on 3/7/15.
+//  Copyright (c) 2015 Duc Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Richard Nguyen on 3/7/15.
+//  Copyright (c) 2015 Duc Nguyen. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
